d6a46a3bd5b2c2a97fd5d803c6bed1deed33edf9
Snahider <snahider@gmail.com>
2013-04-24 07:39:01 -0500

    Actualización de Slides UnitTesting

---
+++ Diapositivas/Codigo/UnitTest/NombreDeLasPruebas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest.Shared;
+
+namespace UnitTest
+{
+    class NombreDeLasPruebas
+    {
+        [TestMethod]
+        public void estaVacioCuandoNoTieneElementos()
+        {
+            Stack stack = new Stack();
+            boolean isEmtpy = stack.IsEmpty;
+            Assert.IsTrue(isEmpty);
+        }
+    }
+}
---
+++ Diapositivas/Codigo/UnitTest/NuestraSegundaPrueba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest.Shared;
+
+namespace UnitTest
+{
+    class NuestraSegundaPrueba
+    {
+
+        [TestMethod]
+        public void noEstaVacioCuandoContieneElementos()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+
+            bool isEmpty = stack.IsEmpty;
+
+            Assert.IsFalse(isEmpty);
+        }
+    }
+}
