d7f4c2889c4557f878f929962ff48892aaf58308
Snahider <snahider@gmail.com>
2013-12-21 11:31:42 -0500

    Instrucciones

--- Ejercicios/7. Web Testing Introduction/Ejercicio/WebTesting.Tests/TranslatorTests.cs
+++ Ejercicios/7. Web Testing Introduction/Ejercicio/WebTesting.Tests/TranslatorTests.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
 namespace WebTesting.Tests
 {
     [TestClass]
     public class TranslatorTests
     {
+        [TestMethod]
+        public void TranslateSpanishToEnglish()
+        {
 
+        }
     }
 }
